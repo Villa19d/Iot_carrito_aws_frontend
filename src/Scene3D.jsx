bd25de89aeaf2c266b9c9c88a0b15f741c48ed50
Rodrigo <luisrodrigo1005@gmail.com>
2026-05-31 23:27:14 -0600

    Fix grid orientation and add synthwave background

--- src/Scene3D.jsx
+++ src/Scene3D.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { Grid, OrbitControls } from '@react-three/drei';
+import { Grid, OrbitControls, Stars } from '@react-three/drei';
 import { Physics, usePlane } from '@react-three/cannon';
 import Vehicle from './Vehicle';
 
@@ -18,18 +18,6 @@ function Ground() {
         <planeGeometry args={[1000, 1000]} />
         <meshBasicMaterial />
       </mesh>
-      <Grid 
-        args={[1000, 1000]} 
-        position={[0, 0.01, 0]}
-        cellSize={1} 
-        cellThickness={1.5} 
-        cellColor="#0ea5e9" 
-        sectionSize={5} 
-        sectionThickness={2.5} 
-        sectionColor="#8b5cf6" 
-        fadeDistance={50} 
-        fadeStrength={1} 
-      />
     </group>
   );
 }
@@ -40,10 +28,31 @@ export default function Scene3D({ lastMovement }) {
   return (
     <Canvas 
       camera={{ position: [5, 5, 10], fov: 45 }} 
-      style={{ width: '100%', height: '100%', background: '#050505' }}
+      style={{ width: '100%', height: '100%', background: '#0f0c29' }}
     >
+      {/* Fondo degradado espacial/synthwave y Niebla para el horizonte */}
+      <color attach="background" args={['#0f0c29']} />
+      <fog attach="fog" args={['#0f0c29', 10, 50]} />
+
       <OrbitControls ref={controlsRef} makeDefault maxPolarAngle={Math.PI / 2 - 0.05} minDistance={3} maxDistance={20} />
       
+      {/* Estrellas de fondo */}
+      <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={1} />
+
+      {/* Grid espectacular estilo Synthwave / Cyberpunk (Fuera del motor de físicas para que quede horizontal) */}
+      <Grid 
+        args={[1000, 1000]} 
+        position={[0, -0.49, 0]}
+        cellSize={1} 
+        cellThickness={1.5} 
+        cellColor="#0ea5e9" 
+        sectionSize={5} 
+        sectionThickness={2.5} 
+        sectionColor="#8b5cf6" 
+        fadeDistance={50} 
+        fadeStrength={1} 
+      />
+
       {/* Iluminación Neon Synthwave: Cero costo gráfico, 100% estilo */}
       <hemisphereLight skyColor="#8b5cf6" groundColor="#000000" intensity={0.6} />
       <directionalLight position={[10, 20, 10]} intensity={1.5} color="#0ea5e9" />
